3bcdc4db2935bb20145210388e5c1552dbdfcf39
david <dechinander@hotmail.com>
2017-07-12 21:16:35 -0600

    DC: Test Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,42 @@
 package main.java;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello Dave!");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
     public static void main(String[] args) {
         launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Label nameLbl = new Label("Enter name");
+        TextField nameFld = new TextField();
+        Label msg = new Label();
+        msg.setStyle("-fx-text-fill: blue;");
+        Button sayHelloBtn = new Button("Say Hello");
+        Button exitBtn = new Button("Exit");
+        sayHelloBtn.setOnAction(e->{
+            String name = nameFld.getText();
+            if(name.trim().length()>0){
+                msg.setText("Hello "+name);
+            }else {
+                msg.setText("Hello you");
+            }
+        });
+        exitBtn.setOnAction(e->Platform.exit());
+
+        VBox root = new VBox();
+        root.setSpacing(5);
+        root.getChildren().addAll(nameLbl, nameFld, msg, sayHelloBtn, exitBtn);
+        Scene scene = new Scene(root, 350, 150);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Improved Hello JavaFX App");
+        primaryStage.show();
     }
 }
